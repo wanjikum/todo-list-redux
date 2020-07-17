66cf10b04806f29954ec3b6beb2ba8ea65d862f9
MillicentNj <millicent.njuguna@allangray.co.za>
2020-07-17 16:27:59 +0300

    feature: add to do item

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { func, string, bool, number, arrayOf, shape } from 'prop-types';
+
+import { addTodoItem } from './redux/actions';
+
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <h1>Hello World!</h1>
-    <h1>Hello World!</h1>
-    <h1>Hello World!</h1>
-    <h1>Hello World!</h1>
-  </div>
-);
+const mapDispatchToProps = (dispatch) => ({
+  handleAddTodoItem(todoItem) {
+    dispatch(addTodoItem(todoItem));
+  },
+});
 
-export default hot(module)(App);
+const mapStateToProps = (state) => {
+  console.log('state>>>', state);
+  return {
+    todos: state.todos,
+  };
+};
+
+const todoItem = { todoItem: '', done: false };
+
+const App = ({ todos, handleAddTodoItem }) => {
+  const [data, setTodoItem] = useState(todoItem);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setTodoItem({ ...data, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleAddTodoItem(data);
+    setTodoItem(todoItem);
+    // alert(JSON.stringify(data));
+  };
+  // const handleChange = () => {};
+
+  return (
+    <div className="app">
+      <h1>A Simple To Do App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="todoItem" onChange={handleChange} value={data.todoItem} />
+        <input type="submit" value="Add Todo Item" />
+        <h2>Todo List</h2>
+        <ul>
+          {todos.map((todo) => (
+            <li>{todo.todoItem}</li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
+};
+
+App.propTypes = {
+  todos: arrayOf(
+    shape({
+      todoItem: string,
+      done: bool,
+      id: number,
+    })
+  ).isRequired,
+  handleAddTodoItem: func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
